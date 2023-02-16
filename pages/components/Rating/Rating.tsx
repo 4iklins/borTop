607cb0a80f305fcfb17f58a3ca65fb333812704e
4iklins <4iklins@gmail.com>
2023-02-16 16:54:42 +0300

    add evt on rating

--- pages/components/Rating/Rating.tsx
+++ pages/components/Rating/Rating.tsx
@@ -1,31 +1,60 @@
 import { RatingProps } from "./Rating.props";
 import styles from "./Rating.module.css";
 import cn from "classnames";
-import { useEffect, useState } from 'react';
-import StarIcon from './star.svg';
+import { KeyboardEvent, useEffect, useState } from "react";
+import StarIcon from "./star.svg";
 
-export const Rating = ({ rating, isEditable = false, setRating, ...props }: RatingProps): JSX.Element => {
+export const Rating = ({rating, isEditable = false, setRating, ...props}: RatingProps): JSX.Element => {
+	const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>));
 
-  const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>));
+	useEffect(() => {
+		constructRating(rating);
+	}, [rating]);
 
-  useEffect(() => {
-    constructRating(rating);
-  }, [rating]);
+	const constructRating = (currentRating: number) => {
+		const updatedArray = ratingArray.map((item: JSX.Element, i: number) => {
+			return (
+				<span
+					className={cn(styles.star, {
+						[styles.filled]: i < currentRating,
+            [styles.isEditable]: isEditable
+					})}
+          onMouseEnter={()=> changeFill(i+1)}
+          onMouseLeave={()=> changeFill(rating)}
+          onClick={()=>onClick(i+1)}
+				>
+					<StarIcon
+          tabIndex={isEditable?0:-1}
+          onKeyDown={(evt)=> onSpacePress(i+1,evt)}
+          />
+				</span>
+			);
+		});
+		setRatingArray(updatedArray);
+	};
 
-  const constructRating = (currentRating: number) => {
-    const updatedArray = ratingArray.map((item: JSX.Element, i: number) => {
-      return <StarIcon className={cn(styles.star, {
-        [styles.filled]: i < currentRating
-      })}
-      />;
-    });
-    setRatingArray(updatedArray);
+  const changeFill = (index: number) => {
+    if(!isEditable){
+      return;
+    }
+    constructRating(index);
+  };
+  const onClick = (index:number) => {
+    if(!isEditable || !setRating){
+      return;
+    }
+    setRating(index);
+  };
+  const onSpacePress = (index:number, evt:KeyboardEvent<SVGAElement>) => {
+    if(evt.code != 'Space' || !setRating){
+      return;
+    }
+    setRating(index);
   };
 
-
-  return (
-    <div {...props}>
-      {ratingArray.map((star, i) => <span key={i}>{star}</span>)}
-    </div>
-  );
+	return (
+		<div {...props}>
+			{ratingArray.map((star, i) => (<span key={i}>{star}</span>))}
+		</div>
+	);
 };
