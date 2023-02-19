ea3c94d9499db1cbac9ec3da2aeb2fba69acba9a
4iklins <52668068+4iklins@users.noreply.github.com>
2023-02-19 23:09:29 +0300

    add layout

--- pages/layout/Layout.tsx
+++ pages/layout/Layout.tsx
@@ -2,16 +2,18 @@ import { LayoutProps } from "./Layout.props";
 import { Header } from "./Header/Header";
 import { Footer } from "./Footer/Footer";
 import { Sidebar } from "./Sidebar/Sidebar";
-import styles from "./Ptag.module.css";
+import styles from "./Layout.module.css";
 import cn from "classnames";
 
-export const Layout = ({children, ...props }: LayoutProps): JSX.Element => {
+export const Layout = ({ children, ...props }: LayoutProps): JSX.Element => {
   return (
-    <div {...props}>
-      <Header/>
-      <Sidebar/>
-      {children}
-      <Footer/>
+    <div className={styles.wrapper} {...props}>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <div className={styles.body}>
+        {children}
+      </div>
+      <Footer className={styles.footer} />
     </div>
   );
 };
